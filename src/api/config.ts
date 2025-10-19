cbe1039d41d9bdf70a254860573ed11f1aea583a
Mohammed Wael <mohammedsama2017@gmail.com>
2025-10-19 18:22:25 +0300

    -

--- src/api/config.ts
+++ src/api/config.ts
@@ -30,7 +30,6 @@ const PUBLIC_ENDPOINT_PREFIXES: string[] = [
   API_ENDPOINTS.login,
   API_ENDPOINTS.signup,
   API_ENDPOINTS.refreshToken,
-  API_ENDPOINTS.lastCourseProgress,
   API_ENDPOINTS.webView,
   API_ENDPOINTS.dashboardStats,
   API_ENDPOINTS.topReviews,
